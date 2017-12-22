f68008888cc0cceb4d0742293c515e1305e55823
Beth Skwerski <Beth.Skwerski@libertymutual.com>
2017-12-22 10:15:16 -0500

    added two way binding to existing child to parent binding

--- src/app/answer/answer.component.ts
+++ src/app/answer/answer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-answer',
@@ -7,9 +7,21 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class AnswerComponent implements OnInit {
 @Input() contestAnswer: string;
- 
+@Output() buttonClicked = new EventEmitter();
+usersAnswer: String;
 
   constructor() { }
+
+  //toLowercase does not like undefined and I didn't like empty strings. SO I came up with this extra check to resolve both items.
+  isPopulated() {
+    if (this.usersAnswer == undefined || this.usersAnswer == "") {
+      alert("Please populate entry field");
+    }
+    else {
+      this.buttonClicked.emit(this.usersAnswer);
+      this.usersAnswer = "";
+    }
+  }
 
   
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {JeopardyService } from './jeopardy.service';
+import { JeopardyService } from './jeopardy.service';
 
 @Component({
   selector: 'app-root',
@@ -7,47 +7,36 @@ import {JeopardyService } from './jeopardy.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
- 
+
 
   questionInfo;
   serviceAnswer: String;
-   usersAnswer: String;
-   userScore: number = 0;
+  userScore: number = 0;
 
-  
-  constructor(private jeopardyService: JeopardyService){}
 
-  getDataFromService(){
+  constructor(private jeopardyService: JeopardyService) { }
+
+  getDataFromService() {
     this.jeopardyService.getQuestionInfo()
       .subscribe(
-        questionInfo => {
-          this.questionInfo = questionInfo[0];
-          this.serviceAnswer = this.questionInfo.answer;
-          
-        }
+      questionInfo => {
+        this.questionInfo = questionInfo[0];
+        this.serviceAnswer = this.questionInfo.answer;
+
+      }
       )
   }
 
-  //toLowercase does not like undefined and I didn't like empty strings. SO I came up with this extra check to resolve both items.
-  isPopulated(){
-    if(this.usersAnswer == undefined||this.usersAnswer == ""){
-      alert("Please populate entry field");
-    }
-    else{
-      this.checkAnswer();
-    }
-  }
-  
-  checkAnswer(){
-    if(this.usersAnswer.toLowerCase()==this.questionInfo.answer.toLowerCase()){
+  checkAnswer(usersAnswer) {
+    if (usersAnswer.toLowerCase() == this.questionInfo.answer.toLowerCase()) {
       this.userScore += this.questionInfo.value;
-     
+
     }
-    this.usersAnswer= "";
+    
     this.getDataFromService();
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.getDataFromService()
   }
 
